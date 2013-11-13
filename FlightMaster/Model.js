9074e8a406a21c7605a56deae382342837fb5848
Gary Pedretty <gary@gary.frontierflying.com>
2013-11-12 16:54:24 -0900

    Initial commit

---
+++ FlightMaster/Model.js
@@ -0,0 +1,10 @@
+﻿model.mergeOutsideCatalog("FlightMaster", {
+hostname: "10.9.1.166:8088",
+user: "",
+password: "",
+// jsFile: "FlightMaster.js",
+timeout:15 } ); 
+
+guidedModel =// @startlock
+{
+};// @endlock
---
+++ FlightMaster/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,42 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var ActiveH6 = {};	// @button
+	var Active7H = {};	// @button
+	var ActiveAll = {};	// @button
+// @endregion// @endlock
+
+	var lastNameSearch = {}; // @search button
+
+// eventHandlers// @lock
+
+	ActiveH6.click = function ActiveH6_click (event)// @startlock
+	{// @endlock
+		filterH6 = "(Active == true && (Company == 'H6' || Company2 == 'H6' || Company3 == 'H6'))";
+		sources.accounts.query(filterH6);
+		sources.accounts.orderBy("Last_Name,First_Name");
+	};// @lock
+
+	Active7H.click = function Active7H_click (event)// @startlock
+	{// @endlock
+		filter7H = "(Active == true && (Company == '7H' || Company2 == '7H' || Company3 == '7H'))";
+		sources.accounts.query(filter7H);
+		sources.accounts.orderBy("Last_Name,First_Name");
+
+	};// @lock
+
+	ActiveAll.click = function ActiveAll_click (event)// @startlock
+	{// @endlock
+		filterAll = "Active == true";
+		sources.accounts.query(filterAll);
+		sources.accounts.orderBy("Last_Name,First_Name");
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("ActiveH6", "click", ActiveH6.click, "WAF");
+	WAF.addListener("Active7H", "click", Active7H.click, "WAF");
+	WAF.addListener("ActiveAll", "click", ActiveAll.click, "WAF");
+// @endregion
+};// @endlock
